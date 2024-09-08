62785a8d82504f6cb0bbcd5c145d509e384b5b26
Gaurav Jaiswal <gauravjaiswal5200@gmail.com>
2024-09-08 11:28:16 +0530

    Rock Paper Scissor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import rock from "./Images/rock.png";
+import paper from "./Images/paper.png";
+import scissor from "./Images/scissor.png";
+import { useState } from "react";
 
 function App() {
+  const [yourScore,setYourScore]=useState(0);
+  const [computerScore,setcomputerScore]=useState(0);
+  const result=["Rock","Paper","Scissor","It's is tie"];
+  const [computerMove,setComputerMove]=useState(null);
+  const handleOnClick=(value)=>{
+    const computerValue=Math.floor(Math.random()*3)+1;
+    setComputerMove(computerValue);
+    if((computerValue === 1 && value===2) || (computerValue === 2 && value===3) || (computerValue === 3 && value===1)){
+     setYourScore(yourScore+1);
+    }
+    else if(computerValue===value){
+     setComputerMove(4);
+    }
+    else{
+      setcomputerScore(computerScore+1);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rock Paper Scissor Game</h1>
+      <div className="container">
+        <h2>Computer Move is: {result[computerMove-1]}</h2>
+        <h2>Choose Your Move:</h2>
+      </div>
+      <div className="images">
+        <button onClick={()=>handleOnClick(1)}>
+          <img src={rock} alt="rock Image" className="rock" />
+        </button>
+        <button onClick={()=>handleOnClick(2)}>
+          <img src={paper} alt="rock Image" className="paper" />
+        </button>
+        <button onClick={()=>handleOnClick(3)}>
+          <img src={scissor} alt="rock Image" className="scissor" />
+        </button>
+      </div>
+      <div className="footer">
+        <h2>Your Score:{yourScore} Computer Score: {computerScore}</h2>
+      </div>
     </div>
   );
 }
